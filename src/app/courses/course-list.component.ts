f835b42e554ea6b7e1d78c5f816055dadeb6996c
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-08-06 17:53:42 -0400

    Injeção de dependência

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,3 +1,4 @@
+import { CourseService } from './course.service';
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 
@@ -10,30 +11,10 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit{
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService){
 
+  }
   ngOnInit(): void{
-    this.courses = [
-      {
-      id: 1,
-      name: 'Angular: Forms',
-      imgUrl: '/assets/images/forms.png',
-      price: 99.99,
-      code: 'XPS',
-      duration: 120,
-      rating: 4.4,
-      releaseDate: 'December, 2, 2020'
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTP',
-        imgUrl: '/assets/images/http.png',
-        price: 79.99,
-        code: 'XHP',
-        duration: 150,
-        rating: 4.0,
-        releaseDate: 'November, 15, 2019'
-        }
-
-  ]
+    this.courses = this.courseService.retrieveAll();
   }
 }
